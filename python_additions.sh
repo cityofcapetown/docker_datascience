0184bc35e009c291c2491aa92608d3599717f953
Gordon Inggs <gordon.e.inggs@ieee.org>
2018-08-20 12:34:23 +0200

    Adding Python modules
    * Geospatial plotting (ipyleaflet)
    * matplotlib - plotting library
    * SciKit Learn - machine learning library
    * SciPy - widely used bindings for LAPACK, BLAS, et al.

--- python_additions.sh
+++ python_additions.sh
@@ -7,5 +7,14 @@ python3 -m pip install boto3
 python3 -m pip install awscli
 python3 -m pip install minio
 python3 -m pip install pyhdb
+python3 -m pip install matplotlib
+python3 -m pip install scipy
+python3 -m pip install sklearn
+python3 -m pip install ipyleaflet
+python3 -m pip install geojson-utils
 
 # Non-standard packages go here
+jupyter nbextension enable --py --sys-prefix ipyleaflet
+jupyter nbextension enable --py widgetsnbextension
+jupyter labextension install jupyter-leaflet
+jupyter labextension install @jupyter-widgets/jupyterlab-manager
