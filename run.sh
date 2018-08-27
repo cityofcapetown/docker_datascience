c1636a10f9936a442200f91d7aaceca1eaa618f3
Riaz Arbi <riazarbi@users.noreply.github.com>
2018-08-27 08:26:17 +0200

    added a SUDO env (#5)
    * added a SUDO env
    
    This environmental variable will be used in an if statement in run.sh to determine whether to grant sudo privileges to the user at runtime.
    
    * Only grant new user sudo if explicitly requested
    
    To grant sudo, set `-e SUDO=yes` in the docker run command

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 useradd -ms /bin/bash $NEWUSER
 # Change user password from default
 echo $NEWUSER:$PASSWD | chpasswd
-# Grant new user sudo
-adduser $NEWUSER sudo
+# Grant new user sudo if ENV says so
+if [[ $SUDO = "yes" ]]; then adduser $NEWUSER sudo fi
 
 # Clone a project git repo into the /home/$NEWUSER folder
 cd /home/$NEWUSER && /usr/bin/git clone $GITREPO
@@ -12,7 +12,7 @@ chown -R $NEWUSER:$NEWUSER /home/$NEWUSER
 
 # Make the new user an admin user of Jupyterhub
 sed -i "/c.Authenticator.admin_users/c\c.Authenticator.admin_users = {'\$NEWUSER\'}" /etc/jupyterhub/jupyterhub_config.py
-#sed -i "/c.Authenticator.admin_users/c\c.Authenticator.admin_users = {\'$username\'}" ~/jupyterhub_config.py
+
 # Run shiny
 if [[ $SHINY = "yes" ]]; then shiny-server &> /dev/null & fi
 # Run Rstudio
@@ -21,4 +21,3 @@ if [[ $RSTUDIO = "yes" ]]; then rstudio-server start &> /dev/null & fi
 if [[ $JUPYTER = "yes" ]]; then jupyterhub -f /etc/jupyterhub/jupyterhub_config.py &> /dev/null & fi
 # Drop privileges to sudo user and enter bash
 read -n 1 -s -r -p "Press any key to quit...  `echo $'\n \n \n> '`"
-
