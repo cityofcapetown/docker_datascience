96676299ed872b8e999fd71a8adbc0d57454dc18
Riaz Arbi <riazarbi@users.noreply.github.com>
2018-12-13 20:51:55 +0200

    Git clone only if repo supplied; try fix permissions [experimental]

--- run.sh
+++ run.sh
@@ -5,10 +5,12 @@ echo $NEWUSER:$PASSWD | chpasswd
 # Grant new user sudo if ENV says so
 if [[ $SUDO = "yes" ]]; then adduser $NEWUSER sudo ; fi
 
-# Clone a project git repo into the /home/$NEWUSER folder
-cd /home/$NEWUSER && /usr/bin/git clone $GITREPO
-# Sort out permissions for git git folder
+# Clone a project git repo into the /home/$NEWUSER folder if env var has been changed
+if [[ $GITREPO != "no" ]]; then cd /home/$NEWUSER && /usr/bin/git clone $GITREPO ; fi
+
+# Sort out permissions for home folder
 chown -R $NEWUSER:$NEWUSER /home/$NEWUSER
+chmod -R 770 /home/$NEWUSER
 
 # Make the new user an admin user of Jupyterhub
 sed -i "/c.Authenticator.admin_users/c\c.Authenticator.admin_users = {'\$NEWUSER\'}" /etc/jupyterhub/jupyterhub_config.py
