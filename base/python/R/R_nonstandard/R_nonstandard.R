6688440b6f587d925e524b71f40e9af2c2f82161
Gordon Inggs <gordon.inggs@capetown.gov.za>
2018-12-17 18:46:42 +0200

    Splitting R image further.
    R_nonstandard should mostly be visualisation and non-CRAN packages.

---
+++ base/python/R/R_nonstandard/R_nonstandard.R
@@ -0,0 +1,63 @@
+# This installs packages that are not present in the base R docker image
+# Will be added to with time
+
+# CRAN packages go here:
+list.of.packages <- c("caTools",
+                      "shinythemes",
+                      "shinyBS",
+                      "shinyLP",
+                      "keras",
+                      "xlsx",
+                      "Quandl",
+                      "network",
+                      "sna",
+                      "visNetwork",
+                      "threejs",
+                      "networkD3",
+                      "ndtv",
+                      "leaflet",
+                      "factoextra",
+                      "arules",
+                      "DiagrammeRsvg", "DiagrammeRsvg",
+                      "arulesViz", "kohonen", "dummies", "tempR", "WDI",
+                      "smacof", "cluster", "ggmap", "googleway", "RJSONIO",
+                      "bupaR", "edeaR", "eventdataR", "processmapR",
+                      "processmonitR", "xesreadR", "petrinetR",
+                      "prophet", "timevis", "kableExtra",
+                      "IRdisplay", "evaluate", "crayon", "pbdZMQ",
+                      "esquisse", "flexdashboard", "shinydashboard", "reticulate")
+
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+
+options(Ncpus = 4)
+if(length(new.packages)) install.packages(new.packages)
+
+# Non-standard packages go here
+
+# INSTALL TINYTEX
+library(devtools)
+library(tinytex)
+tinytex::install_tinytex(force=TRUE)
+
+# INSTALL XARINGAN
+devtools::install_github('yihui/xaringan')
+install.packages("webshot")
+library(webshot)
+install_phantomjs()
+
+# INSTALL DYGRAPHS
+devtools::install_github(c("ramnathv/htmlwidgets", "rstudio/dygraphs"))
+
+# INSTALL KERAS
+devtools::install_github("rstudio/keras")
+library(keras)
+keras::install_keras()
+
+#INSTALL H2O
+# The following two commands remove any previously installed H2O packages for R.
+if ("package:h2o" %in% search()) { detach("package:h2o", unload=TRUE) }
+if ("h2o" %in% rownames(installed.packages())) { remove.packages("h2o") }
+# Now we download and install h2o runtime and R h2o package.
+install.packages("h2o", type="source",
+repos="http://h2o-release.s3.amazonaws.com/h2o/rel-wolpert/8/R")
+
