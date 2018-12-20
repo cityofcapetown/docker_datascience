7ab8c654a719a23008e4598b56169e970a54e79a
Riaz Arbi <riaz.arbi@capetown.gov.za>
2018-12-20 11:51:12 +0200

    Fixes

--- base/python/R/R_nonstandard/rstudio_shiny/run_rstudio.sh
+++ base/python/R/R_nonstandard/rstudio_shiny/run_rstudio.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-# Fixing RStudio redirect
+# Fix NGINX hostname for dynamic routing
 if [[ $VIRTUAL_PATH = "/" ]]; then
-    echo "VIRTUAL PATH IS ROOT. NOT MODIFYING RSTUDIO NGINX CONF..."
+    echo "VIRTUAL PATH IS ROOT. NOT MODIFYING NGINX CONF..."
 else
+    # Fixing RStudio redirect
     sed -i "\/proxy_redirect http\:\/\/localhost\:8787\//c\proxy_redirect http\:\/\/localhost\:8787\/ http\:\/\/\$host$VIRTUAL_PATH\/rstudio\/\;" /etc/nginx/paths-available/rstudio.conf
     sed -i "\/proxy_redirect https\:\/\/localhost\:8787\//c\proxy_redirect https\:\/\/localhost\:8787\/ https\:\/\/\$host$VIRTUAL_PATH\/rstudio\/\;" /etc/nginx/paths-available/rstudio.conf
 fi
