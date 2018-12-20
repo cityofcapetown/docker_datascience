4116183aa450ab328d7dc01672ee40d38e737136
Riaz Arbi <riazarbi@users.noreply.github.com>
2018-12-20 09:03:04 +0200

    Added apt source for R 3.5; default is 3.4

--- base/python/R/apt.sh
+++ base/python/R/apt.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+# Install R
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+echo "# CRAN Repo" | sudo tee -a /etc/apt/sources.list
+echo "deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/" | sudo tee -a /etc/apt/sources.list
+
 DEBIAN_FRONTEND=noninteractive \
   apt-get clean && \
   apt-get update && \
   apt-get install -y \
   r-base \
+  r-base-dev \
   libxml2-dev \
   software-properties-common \
   libssh2-1-dev \
