3731e304fe50f0d2203d5a7f7c4ac2f8499c76fd
Gordon Inggs <gordon.inggs@gmail.com>
2018-12-20 11:57:58 +0200

    Adding back checks NGINX sed checks.

--- base/python/R/R_nonstandard/rstudio_shiny/run_rstudio.sh
+++ base/python/R/R_nonstandard/rstudio_shiny/run_rstudio.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 # Fixing RStudio redirect
-sed -i "\/proxy_redirect http\:\/\/localhost\:8787\//c\proxy_redirect http\:\/\/localhost\:8787\/ http\:\/\/\$host$VIRTUAL_PATH\/rstudio\/\;" /etc/nginx/paths-available/rstudio.conf
-sed -i "\/proxy_redirect https\:\/\/localhost\:8787\//c\proxy_redirect https\:\/\/localhost\:8787\/ https\:\/\/\$host$VIRTUAL_PATH\/rstudio\/\;" /etc/nginx/paths-available/rstudio.conf
+if [[ $VIRTUAL_PATH = "/" ]]; then
+    echo "VIRTUAL PATH IS ROOT. NOT MODIFYING RSTUDIO NGINX CONF..."
+else
+    sed -i "\/proxy_redirect http\:\/\/localhost\:8787\//c\proxy_redirect http\:\/\/localhost\:8787\/ http\:\/\/\$host$VIRTUAL_PATH\/rstudio\/\;" /etc/nginx/paths-available/rstudio.conf
+    sed -i "\/proxy_redirect https\:\/\/localhost\:8787\//c\proxy_redirect https\:\/\/localhost\:8787\/ https\:\/\/\$host$VIRTUAL_PATH\/rstudio\/\;" /etc/nginx/paths-available/rstudio.conf
+fi
 
 echo "Starting RStudio..."
 rstudio-server start &
--- base/python/R/R_nonstandard/rstudio_shiny/run_shiny.sh
+++ base/python/R/R_nonstandard/rstudio_shiny/run_shiny.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 # Fixing Shiny redirect
-sed -i "\/rewrite \^\/shiny\$/c\/rewrite \^\/shiny\$ \$scheme\:\/\/\$http_host$VIRTUAL_PATH\/shiny\/ permanent;"
-sed -i "\/proxy_redirect http\:\/\/localhost\:3838\//c\proxy_redirect http\:\/\/localhost\:3838\/ http\:\/\/\$host$VIRTUAL_PATH\/shiny\/\;" /etc/nginx/paths-available/shiny.conf
-sed -i "\/proxy_redirect https\:\/\/localhost\:3838\//c\proxy_redirect https\:\/\/localhost\:3838\/ https\:\/\/\$host$VIRTUAL_PATH\/shiny\/\;" /etc/nginx/paths-available/shiny.conf
+if [[ $VIRTUAL_PATH = "/" ]]; then
+    echo "VIRTUAL PATH IS ROOT. NOT MODIFYING SHINY NGINX CONF..."
+else
+  sed -i "\/rewrite \^\/shiny\$/c\/rewrite \^\/shiny\$ \$scheme\:\/\/\$http_host$VIRTUAL_PATH\/shiny\/ permanent;"
+  sed -i "\/proxy_redirect http\:\/\/localhost\:3838\//c\proxy_redirect http\:\/\/localhost\:3838\/ http\:\/\/\$host$VIRTUAL_PATH\/shiny\/\;" /etc/nginx/paths-available/shiny.conf
+  sed -i "\/proxy_redirect https\:\/\/localhost\:3838\//c\proxy_redirect https\:\/\/localhost\:3838\/ https\:\/\/\$host$VIRTUAL_PATH\/shiny\/\;" /etc/nginx/paths-available/shiny.conf
+fi
 
 echo "Starting Shiny..."
 shiny-server &
