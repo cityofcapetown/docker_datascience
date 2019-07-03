74bf75ab4aae43cedaf20963511324f2212f6ba0
Gordon Inggs <Gordonei@users.noreply.github.com>
2019-07-03 14:37:50 +0200

    Adding pymysql library
    At the request of Darren Willenberg

--- base/drivers/python/python_additions.sh
+++ base/drivers/python/python_additions.sh
@@ -22,6 +22,7 @@ python3 -m pip install docker
 python3 -m pip install minio
 python3 -m pip install pyhdb
 python3 -m pip install pyodbc
+python3 -m pip install pymysql
 python3 -m pip install pandas
 python3 -m pip install XlsxWriter
 python3 -m pip install openpyxl
