f2abf2d3d14a8ce70e95cba444d134dcf5b1cbc6
Riaz Arbi <riaz.arbi@capetown.gov.za>
2019-07-22 14:35:03 +0200

    working messy jupyter-k8s

---
+++ base/drivers/python/jupyter-k8s/jupyter_additions.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# jupyter
+DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs npm
+npm cache clean -f && \
+  npm install -g configurable-http-proxy
+python3 -m pip install jupyter jupyterlab
+
+# jupyterlab
+jupyter serverextension enable --py jupyterlab --sys-prefix
+
+# jupyterlab-widgets
+jupyter labextension install @jupyter-widgets/jupyterlab-manager
+
+# jupyterhub
+python3 -m pip install jupyterhub
+jupyter labextension install @jupyterlab/hub-extension
+jupyter labextension enable @jupyterlab/hub-extension
+
+# jupyterlab-git
+jupyter labextension install @jupyterlab/git
+python3 -m pip install jupyterlab-git
+jupyter serverextension enable --py jupyterlab_git --sys-prefix
+
+# jupyterlab-dicovery
+python3 -m pip install jupyterlab-discovery
+
+# ipyleaflet
+python3 -m pip install ipyleaflet
+jupyter nbextension enable --py --sys-prefix ipyleaflet
+jupyter nbextension enable --py widgetsnbextension
+jupyter labextension install jupyter-leaflet
+
+
+# jupyter-matplotlib
+python3 -m pip install ipympl
+jupyter labextension install jupyter-matplotlib
