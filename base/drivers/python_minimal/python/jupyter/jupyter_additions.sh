0416ab7b10ed8e4ec57bc56e19be58e6be7501ca
Gordon Inggs <Gordonei@users.noreply.github.com>
2019-09-22 20:00:50 +0200

    Adding plotly libraries for jupyterlab
    As per plotly docs.

--- base/drivers/python_minimal/python/jupyter/jupyter_additions.sh
+++ base/drivers/python_minimal/python/jupyter/jupyter_additions.sh
@@ -34,3 +34,22 @@ jupyter labextension install jupyter-leaflet
 # jupyter-matplotlib
 python3 -m pip install ipympl
 jupyter labextension install jupyter-matplotlib
+
+# plotly
+## Avoid "JavaScript heap out of memory" errors during extension installation
+export NODE_OPTIONS=--max-old-space-size=4096
+
+## jupyterlab renderer support
+jupyter labextension install jupyterlab-plotly@1.1.0 --no-build
+
+## FigureWidget support
+jupyter labextension install plotlywidget@1.1.0 --no-build
+
+## JupyterLab chart editor support (optional)
+jupyter labextension install jupyterlab-chart-editor@1.2 --no-build
+
+## Build extensions (must be done to activate extensions since --no-build is used above)
+jupyter lab build
+
+## Unset NODE_OPTIONS environment variable
+unset NODE_OPTIONS
