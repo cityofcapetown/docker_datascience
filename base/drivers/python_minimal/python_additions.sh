eaf236aa6c874d73d5d68d6b4c960074565b4fe3
Gordon Inggs <Gordonei@users.noreply.github.com>
2021-02-08 12:24:08 +0200

    Updating pandas and pyarrow requirements

--- base/drivers/python_minimal/python_additions.sh
+++ base/drivers/python_minimal/python_additions.sh
@@ -11,8 +11,8 @@ python3 -m pip install pyhdb
 python3 -m pip install pyodbc
 python3 -m pip install numpy
 python3 -m pip install Cython 
-python3 -m pip install pyarrow
-python3 -m pip install pandas
+python3 -m pip install "pyarrow>=3.0.0"
+python3 -m pip install "pandas>=1.2.0"
 python3 -m pip install python-magic
 python3 -m pip install pyhive
 
