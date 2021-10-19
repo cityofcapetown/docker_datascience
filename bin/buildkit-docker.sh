95e18e2d30a8ba25503a622ad58d203765f5a4b8
Gordon Inggs <gordon.inggs@gmail.com>
2021-10-19 22:17:49 +0200

    Bug fix for image build script

--- bin/buildkit-docker.sh
+++ bin/buildkit-docker.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-OPM_DATA_USER=${1}
-OPM_DATA_PASSWORD=${2}
-DOCKER_USER=${3}
-DOCKER_PASS=${4}
-CONTEXT_PATH=${5} # ${PWD}/base
-IMAGE_TAG=${6} # docker.io/cityofcapetown/datascience:base
+OPM_DATA_USER=$1
+OPM_DATA_PASSWORD=$2
+DOCKER_USER=$3
+DOCKER_PASS=$4
+CONTEXT_PATH=$5
+IMAGE_TAG=$6
 
 # Setting up proxy env variables
 export http_proxy=http://${OPM_DATA_USER}:${OPM_DATA_PASSWORD}@internet.capetown.gov.za:8080
@@ -24,8 +24,8 @@ echo '{"auths": {"https://index.docker.io/v1/": {"auth": "'${BASE64_USER_PASS}'"
 # Here we go!
 export BUILDKITD_FLAGS=--oci-worker-no-process-sandbox
 buildctl-daemonless.sh build --frontend dockerfile.v0 \\
-                             --local context=${CONTEXT_PATH} \\
-                             --local dockerfile=${CONTEXT_PATH} \\
+                             --local context="${CONTEXT_PATH}" \\
+                             --local dockerfile="${CONTEXT_PATH}" \\
                              --opt build-arg:http_proxy=${http_proxy} \\
                              --opt build-arg:https_proxy=${http_proxy} \\
                              --opt build-arg:no_proxy=${no_proxy} \\
@@ -33,7 +33,7 @@ buildctl-daemonless.sh build --frontend dockerfile.v0 \\
                              --opt build-arg:HTTPS_PROXY=${http_proxy} \\
                              --opt build-arg:NO_PROXY=${no_proxy} \\
                              --export-cache type=inline \\
-                             --import-cache type=registry,ref=${IMAGE_TAG} \\
-                             --output type=image,name=${IMAGE_TAG},push=true
+                             --import-cache type=registry,ref="${IMAGE_TAG}" \\
+                             --output type=image,name="${IMAGE_TAG}",push=true
 
 exit $?
