05b4103ae405734aed4cbb28b9ebf1946874deb9
Gordon Inggs <gordon.inggs@gmail.com>
2021-10-20 06:50:08 +0200

    Temporarily disabling geocode array package
    Trouble with installing through the proxy

--- base/drivers/python_minimal/python/python_additions.sh
+++ base/drivers/python_minimal/python/python_additions.sh
@@ -73,7 +73,7 @@ python3 -m pip install descartes
 python3 -m pip install geocoder
 python3 -m pip install rtree
 python3 -m pip install geopy
-python3 -m pip install git+https://github.com/opendatadurban/geocode-array.git
+# python3 -m pip install git+https://github.com/opendatadurban/geocode-array.git
 python3 -m pip install mapclassify
 python3 -m pip install cartopy
 
