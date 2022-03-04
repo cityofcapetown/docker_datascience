6e302dea7991982058db774cc9ad18a44ac4e901
Gordon Inggs <gordon.inggs@gmail.com>
2022-03-04 13:58:07 +0200

    Pinning markupsafe version (for jinja)

--- base/drivers/python_minimal/python_additions.sh
+++ base/drivers/python_minimal/python_additions.sh
@@ -6,6 +6,7 @@ python3 -m pip install --upgrade pip setuptools wheel
 python3 -m pip install python-magic
 python3 -m pip install apache-airflow==2.2.3
 python3 -m pip install apache-airflow[kubernetes]==2.2.3
+python3 -m pip install "markupsafe==2.0.1"
 
 # Data Extraction/Managing
 python3 -m pip install minio
